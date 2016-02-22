18223cfc6bd00d81b0be526a95eea29fe259f613
Kneuman0 <kylepneuman@gmail.com>
2016-02-21 19:58:06 -0500

    modified source folder

--- EllipsoidCalculation/src/ellipsoidCalc/InvalidUserInputException.java
+++
@@ -1,7 +0,0 @@
-package ellipsoidCalc;
-
-public class InvalidUserInputException extends Exception {
-     public InvalidUserInputException(String invalidInput){
-    	 super(invalidInput);
-     }
-}

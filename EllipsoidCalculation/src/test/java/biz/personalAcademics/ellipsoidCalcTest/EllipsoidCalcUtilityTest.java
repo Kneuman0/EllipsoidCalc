343e8df0481118ca6a74e0b463463f000c5a2b74
Kneuman0 <kylepneuman@gmail.com>
2016-04-02 14:25:34 -0400

    Commented tests and adjusted variable names to reflect portion of ellipsoid volume

--- EllipsoidCalculation/src/test/java/biz/personalAcademics/ellipsoidCalcTest/EllipsoidCalcUtilityTest.java
+++ EllipsoidCalculation/src/test/java/biz/personalAcademics/ellipsoidCalcTest/EllipsoidCalcUtilityTest.java
@@ -30,28 +30,40 @@ public class EllipsoidCalcUtilityTest {
 		EllipsoidCalcUtility.convertThetaToRadians("abcd", true);
 	}
 	
+	/**
+	 * Using formula for ellipsoid (1/8) * (4/3)pi(abc)
+	 */
 	@Test
 	public void testVolumeEighth(){
-		double quarterOfEllipseVolume = (1/(double)6) * Math.PI * 2 * 2 * 3;
-		assertThat(EllipsoidCalcUtility.getVolume(0, (Math.PI * .5), (Math.PI * .5), 0, 2, 2, 3), is(quarterOfEllipseVolume));
+		double EighthOfEllipseVolume = (1/(double)6) * Math.PI * 2 * 2 * 3;
+		assertThat(EllipsoidCalcUtility.getVolume(0, (Math.PI * .5), (Math.PI * .5), 0, 2, 2, 3), is(EighthOfEllipseVolume));
 	}
 	
+	/**
+	 * Using formula for ellipsoid (1/4) * (4/3)pi(abc)
+	 */
 	@Test
 	public void testVolumeQuarter(){
 		double quarterOfEllipseVolume = (1/(double)3) * Math.PI * 2 * 2 * 3;
 		assertThat(EllipsoidCalcUtility.getVolume(0, (Math.PI * .5), Math.PI, 0, 2, 2, 3), is(quarterOfEllipseVolume));
 	}
 	
+	/**
+	 * Using formula for ellipsoid (1/2) * (4/3)pi(abc)
+	 */
 	@Test
 	public void testVolumeHalf(){
 		double halfOfEllipseVolume = (2/(double)3) * Math.PI * 2 * 2 * 3;
 		assertThat(EllipsoidCalcUtility.getVolume(0, (Math.PI), Math.PI, 0, 2, 2, 3), is(halfOfEllipseVolume));
 	}
 	
+	/**
+	 * Using formula for ellipsoid (4/3)pi(abc)
+	 */
 	@Test
 	public void testVolumeWhole(){
-		double halfOfEllipseVolume = (4/(double)3) * Math.PI * 2 * 2 * 3;
-		assertThat(EllipsoidCalcUtility.getVolume(0, (Math.PI * 2), Math.PI, 0, 2, 2, 3), is(halfOfEllipseVolume));
+		double fullEllipseVolume = (4/(double)3) * Math.PI * 2 * 2 * 3;
+		assertThat(EllipsoidCalcUtility.getVolume(0, (Math.PI * 2), Math.PI, 0, 2, 2, 3), is(fullEllipseVolume));
 	}
 
 }
